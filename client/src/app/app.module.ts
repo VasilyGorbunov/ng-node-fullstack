1b33223b8f19591f2188fc76ad14886e0f80a9ba
VasilyGorbunov <vasily.gorbunov@gmail.com>
2020-01-22 15:56:20 +0300

    36. Форма логина

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 import {AppRoutingModule} from './app-routing.module'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import {AppComponent} from './app.component'
-import {LoginPageComponent} from './login-page/login-page.component';
-import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
-import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
-import { RegisterPageComponent } from './register-page/register-page.component'
+import {LoginPageComponent} from './login-page/login-page.component'
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component'
+import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component'
+import {RegisterPageComponent} from './register-page/register-page.component'
 
 @NgModule({
   declarations: [
@@ -18,6 +19,8 @@ import { RegisterPageComponent } from './register-page/register-page.component'
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
