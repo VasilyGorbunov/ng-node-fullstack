3f8ccc96d056679734af7d8bc9c52edc176f5e55
VasilyGorbunov <vasily.gorbunov@gmail.com>
2020-01-22 17:11:36 +0300

    37. Сервис авторизации

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 import {AppRoutingModule} from './app-routing.module'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
 
 import {AppComponent} from './app.component'
 import {LoginPageComponent} from './login-page/login-page.component'
@@ -21,6 +22,7 @@ import {RegisterPageComponent} from './register-page/register-page.component'
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,16 +1,25 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnDestroy, OnInit} from '@angular/core'
 import {FormControl, FormGroup, Validators} from '@angular/forms'
+import {Subscription} from 'rxjs'
+import {ActivatedRoute, Params, Router} from '@angular/router'
+
+import {AuthService} from '../shared/services/auth.service'
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup
+  aSub: Subscription
 
-  constructor() {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
   }
 
   ngOnInit() {
@@ -24,6 +33,14 @@ export class LoginPageComponent implements OnInit {
         Validators.minLength(6)
       ])
     })
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if(params['registered']) {
+
+      } else if (params['accessDenied']) {
+
+      }
+    })
   }
 
   get _email() {
@@ -35,6 +52,22 @@ export class LoginPageComponent implements OnInit {
   }
 
   onSubmit() {
+    this.form.disable()
+    this.aSub = this.auth.login(this.form.value)
+      .subscribe(
+        () => {
+          this.router.navigate(['/overview'])
+        },
+        error => {
+          console.warn(error)
+          this.form.enable()
+        }
+      )
+  }
 
+  ngOnDestroy(): void {
+    if (this.aSub) {
+      this.aSub.unsubscribe()
+    }
   }
 }
